76daac19c08698db28ec4e8c0d90b5749f2868b8
Isleimar Oliveira <isleimar@hotmail.com>
2019-05-06 19:56:52 -0300

    Criação do algoritmo de gerar a WordList

--- criarWordList.py
+++ criarWordList.py
@@ -6,46 +6,63 @@ Created on Mon May  6 15:39:31 2019
 @author: isleimar
 """
 
+import random
+
+#Lista de caracteres possíveis
 consoantes=['b','c','d','f','g','h','j','l','m','n','p','q','r','s','t','v','x','z','k','y']
 vogais=['a','e','i','o','u']
 limAfa=[len(consoantes)-1,len(vogais) -1]
 limNum=[7,7]
+wordlist = []
 
+#Configuração da palavra
 palavra=[[0,0],[0,0],[0,0],[0,0]]
+limites=[limAfa,limAfa,limAfa,limNum]
 
-def incSilaba(m,n):
-    if(m[1]<n[1]):
-        m[1] = m[1] +1
-    else:
-        m[1] = 0
-        if(m[0]<n[0]):
-            m[0] = m[0] +1
-        else:
-            m[0] = 0
-            return 1
-    return 0
 
-def incPalavra(g):
-    if (g <= 2 ):
-        if(incSilaba(palavra[g],limAfa)==1):               
-            return incPalavra(g+1)
-    else:
-        return 0
-            
+      
+#Função para montar a saída da sílaba      
 def saidaSilaba(s):
     return consoantes[s[0]] + vogais[s[1]]
         
+#Função para montar a saída da palavra
 def saidaPalavra(p):
+    tamPalavra = len(palavra)-1
     r = ''
-    for j in range(3):
+    for j in range(tamPalavra):
         r = r + saidaSilaba(p[j])        
-    r = r +  str(p[3][0]) + str(p[3][1])
+    r = r +  str(p[tamPalavra][0]) + str(p[tamPalavra][1])
     return r
-cont = True
-while cont:
-    print(saidaPalavra(palavra))
-    if (incPalavra(0) == 0):
-        cont = False;
 
+def aletSilaba(m,n):
+    m[0]= random.randint(0,n[0])
+    m[1]= random.randint(0,n[1])
     
+def aleatorio():
+    tamPalavra = len(palavra)
+    for j in range(tamPalavra):
+        aletSilaba(palavra[j],limites[j])
+        
+def existe(p):
+    for j in wordlist:
+        if j == p:
+            print(p)
+            return True
+    return False
+
+cont = True
+while len(wordlist) < 10000:
+    aleatorio()
+    s = saidaPalavra(palavra)
+    while (existe(s)):
+        aleatorio()
+        s = saidaPalavra(palavra)
+        
+    wordlist.append(s)
+
+arquivo = open('wordlist.txt','w')  
+for j in wordlist:
+    arquivo.writelines(j+'\n')    
+arquivo.close
+print('Fim')
 
